008fdc50fdddc115af9f6fc12a3ac1f7a03f8f67
Louka Gauthier <loukagauthier55@gmail.com>
2024-03-28 15:06:18 -0400

    Nettoyage fichier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroComponent } from './hero/hero.component';
-import { EmotionsComponent } from './emotions/emotions.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { StrengthsComponent } from './strengths/strengths.component';
-import { ProjectProcessComponent } from './project-process/project-process.component';
-import { FAQComponent } from './faq/faq.component';
 import { BookAppointmentComponent } from './book-appointment/book-appointment.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -18,11 +15,8 @@ import { PortfolioItemsComponent } from './portfolio/portfolio-items/portfolio-i
   declarations: [
     AppComponent,
     HeroComponent,
-    EmotionsComponent,
     AboutUsComponent,
     StrengthsComponent,
-    ProjectProcessComponent,
-    FAQComponent,
     BookAppointmentComponent,
     PortfolioComponent,
     FooterComponent,
